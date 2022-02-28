ae721e52334dcb6e2609f9ca5f22b2bd406cfcab
JT Raber <jmann240raber@gmail.com>
2022-02-27 20:37:15 -0500

    added route for reading items

--- main.py
+++ main.py
@@ -69,12 +69,15 @@ async def search(request: Request):
 
 @api.get("/donors", response_model=list[schemas.Donor])
 def read_donors(db: Session = Depends(get_db)):
-    donors = crud.get_donors(db)
-    return donors
+    return crud.get_donors(db)
 
 @api.post("/donors", response_model=schemas.Donor)
 def create_donor(donor: schemas.DonorCreate, db: Session = Depends(get_db)):
     return crud.create_donor(db, donor=donor)
+
+@api.get("/items", response_model=list[schemas.Item])
+def read_items(db: Session = Depends(get_db)):
+    return crud.get_items(db)
 
 @api.post("/items", response_model=schemas.Item)
 def create_item(item: schemas.ItemCreate, db: Session = Depends(get_db)):
