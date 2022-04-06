2f2a9ff33e5a99e5aa1c522a2e10df8b3cdd4c94
Ty Poorman <github@flotts.io>
2022-04-06 17:27:35 -0400

    formatting

--- static/js/managers.js
+++ static/js/managers.js
@@ -4,28 +4,28 @@ var box = document.getElementById("box");
 var addBtn = document.getElementById("add-entry");
 var submitBtn = document.getElementById("submit");
 
-// Get the <span> element that closes the modal
+// get the <span> element that closes the modal
 var close = document.getElementById("close");
 
-// When the user clicks on the button, open the modal
+// when the user clicks on the add manager button, open the modal
 addBtn.onclick = function() {
-  box.style.display = "none";
-  modal.style.display = "block";
-  addBtn.style.opacity = 0;
-  
+    box.style.display = "none";
+    modal.style.display = "block";
+    addBtn.style.opacity = 0;
 }
 
-//close the modal
+// close the modal
 close.onclick = function() {
-  modal.style.display = "none";
-  box.style.display = "flex";
-  addBtn.style.opacity = 100;
+    modal.style.display = "none";
+    box.style.display = "flex";
+    addBtn.style.opacity = 100;
 }
 
+// add manager
 submit.onclick = function() {
-  //NEED TO COLLECT INFO FROM HERE
-  modal.style.display = "none";
-  box.style.display = "flex";
-  addBtn.style.opacity = 100;
+    modal.style.display = "none";
+    box.style.display = "flex";
+    addBtn.style.opacity = 100;
+    
 }
 
