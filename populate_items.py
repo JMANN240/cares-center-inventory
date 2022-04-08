879b52c50b116d2262b4a72315e4a79711916cc5
JT Raber <jmann240raber@gmail.com>
2022-04-07 23:47:06 -0400

    changed to single quantity

--- populate_items.py
+++ populate_items.py
@@ -4,6 +4,6 @@ import schemas
 from database import SessionLocal
 
 with SessionLocal() as db:
-    crud.create_item(db, schemas.ItemCreate(item_name="Beans", item_points=5, item_front_quantity=10, item_back_quantity=5, donor_id=1))
-    crud.create_item(db, schemas.ItemCreate(item_name="Cake pop", item_points=10, item_front_quantity=10, item_back_quantity=10, donor_id=1))
-    crud.create_item(db, schemas.ItemCreate(item_name="Bread", item_points=2, item_front_quantity=10, item_back_quantity=10, donor_id=2))
+    crud.create_item(db, schemas.ItemCreate(item_name="Beans", item_points=5, item_quantity=10, donor_id=1))
+    crud.create_item(db, schemas.ItemCreate(item_name="Cake pop", item_points=10, item_quantity=10, donor_id=1))
+    crud.create_item(db, schemas.ItemCreate(item_name="Bread", item_points=2, item_quantity=10, donor_id=2))
